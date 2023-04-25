14800045d2ce74a7dcd9b4adfd0c50ee68fbca16
John Nixon <johnbrightnixon@gmail.com>
2023-04-25 11:06:19 +0530

    changes description of website

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
@@ -6,6 +7,9 @@ import './App.scss';
 
 const App = () => (
   <div className="app">
+    <Helmet>
+      <meta name="description" content="Transform your business with cutting-edge technology solutions from our full-stack development company. We specialize in creating custom web and mobile apps, as well as providing blockchain development services and SaaS solutions. Our team of experts uses the latest tools and technologies to build scalable and secure solutions tailored to your unique needs. Get started today and take your business to the next level with us" />
+    </Helmet>
     <Navbar />
     <Header />
     <About />
